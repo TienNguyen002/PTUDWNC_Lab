389ee782c74ca26055168b9c4e20d10c1a653d96
TienNguyen002 <2015749@dlu.edu.vn>
2023-03-07 16:11:18 +0700

    Update - Access Data with DI Container / Add to DB

--- src/TipsAndTricks/TatBlog.WebApp/Program.cs
+++ src/TipsAndTricks/TatBlog.WebApp/Program.cs
@@ -1,7 +1,20 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using Microsoft.EntityFrameworkCore;
+using TatBlog.Data.Contexts;
+using TatBlog.Data.Seeders;
+using TatBlog.Services.Blogs;
+
+var builder = WebApplication.CreateBuilder(args);
 {
     //Thêm các dịch vụ được yêu cầu bới MVC Framework
     builder.Services.AddControllersWithViews();
+
+    //Đăng ký các dịch vụ với DI Container
+    builder.Services.AddDbContext<BlogDbContext>(options =>
+        options.UseSqlServer(
+            builder.Configuration.GetConnectionString("DefaultConnection")));
+
+    builder.Services.AddScoped<IBlogRepository, BlogRepository>();
+    builder.Services.AddScoped<IDataSeeder, DataSeeder>();
 }
 
 var app = builder.Build();
@@ -39,4 +52,10 @@ var app = builder.Build();
         pattern: "{controller=Blog}/{action=Index}/{id?}");
 }
 
+//Thêm dữ liệu mẫu vào CSDL
+using (var scope = app.Services.CreateScope())
+{
+    var seeder = scope.ServiceProvider.GetRequiredService<IDataSeeder>();
+    seeder.Initialize();
+}
 app.Run();
