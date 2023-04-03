fbe8bcef855341cf3c3201b5ab92e1d32ac4108e
TienNguyen002 <2015749@dlu.edu.vn>
2023-04-03 14:54:13 +0700

    Done Lab05-B3-Create Pages(About, Contact, RSS)

--- src/tat-blog/src/App.js
+++ src/tat-blog/src/App.js
@@ -2,26 +2,40 @@ import './App.css';
 import Navbar from './Components/Navbar';
 import Sidebar from './Components/Sidebar';
 import Footer from './Components/Footer';
+import Layout from './Pages/Layout';
+import Index from './Pages/Index';
+import About from './Pages/About';
+import Contact from './Pages/Contact';
+import Rss from './Pages/Rss';
 import {
-  BrowserRouter as Router,
+  BrowserRouter as Router, Route, Routes,
 } from 'react-router-dom'
 
-function App(){
+function App() {
   return (
     <div>
+
       <Router>
         <Navbar />
         <div className='container-fluid'>
           <div className="row">
             <div className="col-9">
-
+              <Routes>
+                <Route path='/' element={<Layout/>}>
+                  <Route path='/' element={<Index/>}/>
+                  <Route path='blog' element={<Index/>}/>
+                  <Route path='blog/Contact' element={<Contact/>}/>
+                  <Route path='blog/About' element={<About/>}/>
+                  <Route path='blog/RSS' element={<Rss/>}/>
+                </Route>
+              </Routes>
             </div>
             <div className='col-3 border-start'>
-              <Sidebar/>
+              <Sidebar />
             </div>
           </div>
         </div>
-        <Footer/>
+        <Footer />
       </Router>
     </div>
   );
