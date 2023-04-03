37a237b6c203beea8bcb4c9de7dd1cb42a0cfb48
TienNguyen002 <2015749@dlu.edu.vn>
2023-04-03 15:25:21 +0700

    Done Lab5-B4-Create TagList & PostItem

--- src/tat-blog/src/Pages/Index.js
+++ src/tat-blog/src/Pages/Index.js
@@ -1,14 +1,24 @@
-import React, {useEffect} from "react";
+import React, {useEffect, useState} from "react";
+import PostItem from '../Components/PostItem';
 
 const Index = () => {
+    const [postList, setPostList] = useState([]);
     useEffect(() => {
         document.title = 'Trang chủ';
     }, []);
 
+    if(postList.length > 0)
     return (
-        <h1>
-            Đây là trang chủ
-        </h1>
+        <div className="p-4">
+            {postList.map(item => {
+                return (
+                    <PostItem postItem={item}/>
+                );
+            })};
+        </div>
+    );
+    else return (
+        <></>
     );
 }
 
