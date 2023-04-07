71324e372d4d8bbe3baa66c87eb7c19f77eb4551
TienNguyen002 <2015749@dlu.edu.vn>
2023-04-07 21:53:46 +0700

    Done Lab5-C1-Create Friendly Url

--- src/tat-blog/src/Components/PostItem.js
+++ src/tat-blog/src/Components/PostItem.js
@@ -8,37 +8,44 @@ const PostList = ({postItem}) => {
         ? process.env.PUBLIC_URL + '/images/nopicture.png'
         : `https://localhost:7251/${postItem.imageUrl}`;
     let postedDate = new Date(postItem.postedDate);
+    const {title, shortDescription, category, author, tags} = postItem;
     return (
         <article className='blog-entry mb-4'>
             <Card>
                 <div className='row g-0'>
                     <div className='col-md-4'>
-                        <Card.Img variant='top' src={imageUrl} alt={postItem.title}/>
+                        <Card.Img variant='top' src={imageUrl} alt={title}/>
                     </div>
                     <div className='col-md-8'>
                         <Card.Body>
-                            <Card.Title>{postItem.title}</Card.Title>
+                            <Card.Title>{title}</Card.Title>
                             <Card.Text>
                                 <small className='text-muted'>Tác giả:</small>
-                                <span className='text-primary m-1'>
-                                    {postItem.author.fullName}
+                                <span className='text-primary m-1 '>
+                                    <Link className="text-decoration-none"
+                                    to={`/blog/author/${author.urlSlug}`}>
+                                        {author.fullName}
+                                    </Link>
                                 </span>
                                 <small className='text-muted'>Chủ đề:</small>
                                 <span className='text-primary m-1'>
-                                    {postItem.category.name}
+                                <Link className="text-decoration-none"
+                                    to={`/blog/category/${category.urlSlug}`}>
+                                        {category.name}
+                                    </Link>
                                 </span>
                             </Card.Text>
                             <Card.Text>
-                                {postItem.shortDescription}
+                                {shortDescription}
                             </Card.Text>
                             <div className='tag-list'>
-                                <TagList tagList={postItem.tags}/>
+                                <TagList tagList={tags}/>
                             </div>
                             <div className='text-end'>
                                 <Link
-                                to={`/blog/post?year=${postedDate.getFullYear()}&month=${postedDate.getMonth()}&day=${postedDate.getDay()}&slug=${postItem.urlSlug}`}
+                                to={`/blog/post/year=${postedDate.getFullYear()}/${postedDate.getMonth()}/${postedDate.getDay()}/${postItem.urlSlug}`}
                                 className='btn btn-primary'
-                                title={postItem.title}>
+                                title={title}>
                                     Xem chi tiết
                                 </Link>
                             </div>
